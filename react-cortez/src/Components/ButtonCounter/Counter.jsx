eb88a970fd69da0ce74406628899dc91b946f383
Joaquin Cortez <joaaquincortezz@gmail.com>
2022-04-24 18:01:26 -0300

    button-counter

---
+++ react-cortez/src/Components/ButtonCounter/Counter.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import "./CounterStyle.css";
+
+
+function ItemCount({stock, initial, onAdd}){
+
+    const [count, setCount] = useState(initial);
+
+    function handlePlusButton (){
+
+        if (count < stock){
+
+           setCount(count + 1)
+        }
+    }
+
+    function handleMinusButton(){
+
+        if(count > 0){
+
+            setCount(count - 1)
+        }
+    }
+
+    return(
+        <div className="cardCounter">
+            <div>
+                <button onClick={ () => handleMinusButton()}>-</button>
+                <input readOnly value= {count}/>
+                <button onClick={ () => handlePlusButton()}>+</button>
+            </div>
+            <button className="addToCart" onClick={() =>(count <= stock) && onAdd()}>Agregar al carrito</button>
+        </div>
+    )
+
+}
+
+
+
+
+
+
+
+
+
+
+export default ItemCount;
--- react-cortez/src/Components/ItemListContainer/index.jsx
+++ react-cortez/src/Components/ItemListContainer/index.jsx
@@ -1,9 +1,21 @@
 import React from'react';
+import './item-container.css';
+import ItemCount from '../ButtonCounter/Counter';
+
 
 const ItemListContainer = ({greeting}) =>{
 
+    function purchasedTest(){
+
+        console.log("Se añadio al carrito");
+    }
+
     return(
-        <div> {greeting} </div>
+        <div className='list'>
+            <ItemCount initial={0} stock={10} onAdd={purchasedTest}/>
+        
+        </div>
+        
     )
 }
 
